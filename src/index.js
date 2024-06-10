50147952ec3d14ceea8fea1ad80594a3cf683ff1
Zain Mateen <zaini_2223@yahoo.com>
2024-06-10 15:59:30 +0500

    convert our context file into react redux

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
-import { UserProvider } from './contexts/user.context';
-import { CategoriesProvider } from './contexts/categories.context';
-import { CartProvider } from './contexts/cart.context';
+import { Provider } from 'react-redux';
+import { store } from './store/store'
 
 import './index.scss';
 
-const rootElement = document.getElementById('root');
+import reportWebVitals from './reportWebVitals';
 
-render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+    <Provider store={store}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
 );
+
+reportWebVitals();
---
+++ src/store/categories/categories.types.js
@@ -0,0 +1,8 @@
+
+const CATEGORIES_ACTION_TYPES = {
+    SET_CATEGORIES_MAP: 'categories/SET_CATEGORIES_MAP',
+    SET_CATEGORIES: 'categories/SET_CATEGORIES',
+  };
+  
+  export default CATEGORIES_ACTION_TYPES;
+  
